c251646cb89514c77b6ecb27277388573ae5677e
Jenny Gao <jgao658@gmail.com>
2016-12-26 16:42:03 -0500

    renamed dictionaryWordList to dictWordList

--- Scrabble.java
+++ Scrabble.java
@@ -2,14 +2,14 @@ import java.util.*; //random, scanner, arraylist
 import java.io.*; //file, filenotfoundexception
 
 public class Scrabble{
-    public static ArrayList<String> dictionaryWordList=new ArrayList<String>();
+    public static ArrayList<String> dictWordList=new ArrayList<String>();
     public void initializeArrayList(){
 	try{
 	    Scanner in = new Scanner(new File("WordList.txt"));
 	    while(in.hasNext()){
 		String word = in.next();
 		String wordUpperCase=word.toUpperCase();
-		dictionaryWordList.add(wordUpperCase);
+		dictWordList.add(wordUpperCase);
 	    }
 	}
 	catch(FileNotFoundException e){
@@ -19,9 +19,9 @@ public class Scrabble{
     }
     public static boolean wordValidityCheck(String playersWord){
 	boolean status=false;
-	int listSize=dictionaryWordList.size();
+	int listSize=dictWordList.size();
 	for(int i=0;i<listSize;i++){
-	    if(dictionaryWordList.get(i).equals(playersWord.toUpperCase())){
+	    if(dictWordList.get(i).equals(playersWord.toUpperCase())){
 		status=true;
 	    }
 	    //System.out.println(word.toUpperCase());
@@ -31,7 +31,7 @@ public class Scrabble{
     public static void main(String[] args){
 	Scrabble a=new Scrabble();
 	a.initializeArrayList();
-	//System.out.println(a.dictionaryWordList);
+	System.out.println(a.dictWordList);
 	System.out.println(wordValidityCheck("hello"));
     }
 }
