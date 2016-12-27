5e55e1efc9f675e92f76b8078c3835b752a46414
Jenny Gao <jgao658@gmail.com>
2016-12-27 16:40:35 -0500

    added methods

--- Tile.java
+++ Tile.java
@@ -1,4 +1,26 @@
+import java.util.*;
 public class Tile{
     private String letter;
     private int points;
+    private static String[] alphabet=new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private static ArrayList<String> alphabetLetters=new ArrayList<String>();
+    public void InitializeArrList(){
+	for(int i=0;i<26;i++){
+	    alphabetLetters.add(alphabet[i]);
+	}
+    }
+    private static int[] pointValues=new int[]{1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    public Tile(String a){
+	letter=a;
+	int alphaPos=alphabetLetters.indexOf(a);
+	points=pointValues[alphaPos];
+    }
+    public static void main(String[] args){
+	Tile c=new Tile("B");
+	System.out.println(c.letter);
+	c.InitializeArrList();
+	System.out.println(c.alphabetLetters);
+	System.out.println(c.points);
+    }
+				       
 }
