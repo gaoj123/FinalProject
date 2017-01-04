c1016294e35f5c13c74e425f06616cd581dcc885
Jenny Gao <jgao658@gmail.com>
2017-01-03 19:12:33 -0500

    added constructor

--- TileBag.java
+++ TileBag.java
@@ -1,3 +1,12 @@
+import java.util.*;
 public class TileBag{
-    ArrayList<Tiles> a;
+    private ArrayList<Tile> a;
+    private static String[] alphabet=new String[]{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    public TileBag(){
+	this.a=new ArrayList<Tile>();
+	for(int A=0;A<9;A++){
+	    Tile aa=new Tile("a");
+	    a.add(aa);
+	}
+    }
 }
