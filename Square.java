0f8e12eb9988c2daff09f42eda7d31ac5029cd1f
Winnie Chen <wchen10@stuy.edu>
2017-01-06 06:24:55 -0500

    added placeTile and toString methods. planning on changing instance variable to a Tile.

--- Square.java
+++ Square.java
@@ -13,10 +13,22 @@ public class Square{
 	letter = "";
     }
 
+    public void placeTile(Tile tile){
+	letter = tile.getLetter();
+    }
+
+    public String toString(){
+	return letter + " ";
+    }
+
     public static void main(String[] args){
 	Square s00 = new Square();
 	System.out.println("'" + s00.getLetter() + "'");
 	s00.setLetter("?");
 	System.out.println("'" + s00.getLetter() + "'");
+	Tile tileA = new Tile("A");
+	s00.placeTile(tileA);
+	System.out.println("'" + s00.getLetter() + "'");
+	System.out.println("'" + s00 + "'");
     }
 }
