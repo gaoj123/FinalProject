069844b61c3021b8461c0e89ea9e16e8e36a0483
Jenny Gao <jgao658@gmail.com>
2017-01-06 15:05:08 -0500

    added default constructor

--- Tile.java
+++ Tile.java
@@ -10,6 +10,10 @@ public class Tile{
     	}
     }
     private static int[] pointValues=new int[]{1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    public Tile(){
+	points=0;
+	letter="";
+    }
     public Tile(String a){
 	this.InitializeArrList();
 	letter=a;
