1bcdf986dd5e944afaf870bcf03f3f8ee9ee68a7
Jenny Gao <jgao658@gmail.com>
2017-01-06 21:56:53 -0500

    added another way to add to rack.  i.e. by index

--- Player.java
+++ Player.java
@@ -4,20 +4,38 @@ public class Player{
     ArrayList<Tile> rack;
     int totalScore;
     int roundScore;
+    boolean endTurn;
     public Player(String name){
 	this.name=name;
 	totalScore=0;
 	roundScore=0;
 	rack=new ArrayList<Tile>();
+	endTurn=false;
+    }
+    public void placeWord(String word,int x,int y,String direction){
+    }
+    public Tile tileAtRackIndex(int indexx){
+	return rack.get(indexx);
     }
     public void addToRack(Tile fromBag){
 	rack.add(fromBag);
+    }
+    public void addToRack(int pos, Tile fromBag){
+	rack.add(pos,fromBag);
     }
     public void removeFromRack(Tile toRemove){
 	rack.remove(toRemove);
     }
     public int getRackSize(){
 	return rack.size();
+    }
+    public void requestExchange(){
+	roundScore=0;
+	endTurn=true;
+    }
+    public void pass(){
+	roundScore=0;
+	endTurn=true;
     }
     public String toString(){
 	String retStr="Your tiles: ";
