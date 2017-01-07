deaa33941fd1abed8e6204c8ea949ef1d7b24b9c
Jenny Gao <jgao658@gmail.com>
2017-01-06 21:58:06 -0500

    added alternative to exchanging tiles (by index of tile in rack instead of by letters to be exchanged)

--- TileBag.java
+++ TileBag.java
@@ -50,7 +50,16 @@ public class TileBag{
 	currentplayer.addToRack(a.get(randomIndex));
 	a.remove(randomIndex);
     }
-	
+    public void drawTile(int position, Player currentplayer){
+	if(a.size()==0){
+	    System.out.println("The tile bag is empty, so no additional tiles were added to your rack");
+	}
+	int randomIndex= (int) (Math.random()*(a.size()-1));
+	System.out.println("random index: "+randomIndex);
+	System.out.println(position+"");
+	currentplayer.addToRack(position, a.get(randomIndex));
+	a.remove(randomIndex);
+    }	
     public void refillRack(Player currentplayer){
 	if(a.size()==0){
 	    System.out.println("The tile bag is empty, so no additional tiles were added to your rack");
@@ -68,31 +77,43 @@ public class TileBag{
 	    }
 	}
     }
-    public void exchange(Player currentplayer, ArrayList<Tile> tilesToBeExchanged){
-	if(a.size()<7){
-	    System.out.println("Sorry.  You can't exchange tiles because there are less than 7 tiles left in the tile bag.");
-	}
-	else{
-	    int numRequested=0;
-	    numRequested=tilesToBeExchanged.size();
-	    for(int start=0;start<numRequested;start++){
-		int indexToAddRandomlyBack= (int) (Math.random()*(a.size()-1));
-		Tile examining=tilesToBeExchanged.get(start);
-		a.add(indexToAddRandomlyBack, examining);
-		//	tilesToBeExchanged.remove(start);
-		currentplayer.removeFromRack(examining);
-	    }
-	    for(int i=0;i<numRequested;i++){
-		this.drawTile(currentplayer);
-		//Tile tileClaimed=a.get(0);
-		//currentplayer.addToRack(tileClaimed);
-		//a.remove(z);
-	    }
-	    // for(int j=0;j<numRequested;j++){
-	    // 	tilesToBeExchanged.remove(j);
-	    // }
-	}
-    }
+    // public void exchange(Player currentplayer, ArrayList<Tile> tilesToBeExchanged){
+    // 	if(a.size()<7){
+    // 	    System.out.println("Sorry.  You can't exchange tiles because there are less than 7 tiles left in the tile bag.");
+    // 	}
+    // 	else{
+    // 	    int numRequested=0;
+    // 	    numRequested=tilesToBeExchanged.size();
+    // 	    for(int start=0;start<numRequested;start++){
+    // 		int indexToAddRandomlyBack= (int) (Math.random()*(a.size()-1));
+    // 		Tile examining=tilesToBeExchanged.get(start);
+    // 		a.add(indexToAddRandomlyBack, examining);
+    // 		//	tilesToBeExchanged.remove(start);
+    // 		currentplayer.removeFromRack(examining);
+    // 	    }
+    // 	    for(int i=0;i<numRequested;i++){
+    // 		this.drawTile(currentplayer);
+    // 		//Tile tileClaimed=a.get(0);
+    // 		//currentplayer.addToRack(tileClaimed);
+    // 		//a.remove(z);
+    // 	    }
+    // 	    // for(int j=0;j<numRequested;j++){
+    // 	    // 	tilesToBeExchanged.remove(j);
+    // 	    // }
+    // 	}
+    // }
+       public void exchange(Player currentplayer, int input){
+	   if(a.size()<7){
+	       System.out.println("Sorry.  You can't exchange tiles because there are less than 7 tiles left in the tile bag.");
+	   }
+	   else{
+	       int indexToAddRandomlyBack= (int) (Math.random()*(a.size()-1));
+	       Tile examining=currentplayer.tileAtRackIndex(input-1);
+	       a.add(indexToAddRandomlyBack, examining);
+	       currentplayer.removeFromRack(examining);
+	       this.drawTile(input-1,currentplayer);
+	   }
+       } 
     public static void main(String[] args){
 	Player jen=new Player("Jenny");
 	TileBag test=new TileBag();
@@ -104,11 +125,13 @@ public class TileBag{
 	Tile e=new Tile("e");
 	Tile g=new Tile("g");
 	Tile h=new Tile("h");
-	jen.addToRack(f);
-	jen.addToRack(b);
-	jen.addToRack(c);
-	jen.addToRack(d);
-	jen.addToRack(e);
+	test.refillRack(jen);
+	System.out.println(jen);
+	//jen.addToRack(f);
+	//jen.addToRack(b);
+	//jen.addToRack(c);
+	//jen.addToRack(d);
+	//jen.addToRack(e);
 	//jen.addToRack(g);
 	//jen.addToRack(h);
 	System.out.println(jen.getRackSize());
@@ -126,7 +149,9 @@ public class TileBag{
 	// testLettersToExchange.add(e);
 	//System.out.println(jen);
 	//test.exchange(jen,testLettersToExchange);
-	test.refillRack(jen);
+	test.exchange(jen,5);
+	//System.out.println(jen);
+	//	test.refillRack(jen);
 	System.out.println(jen);
 
 }
