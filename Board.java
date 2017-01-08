0e7c047e9956f13d132b0b3031281ad6da88a0d4
Winnie Chen <wchen10@stuy.edu>
2017-01-08 09:54:22 -0500

    added triple letters to constructor. Need accessing and mutating methods for board

--- Board.java
+++ Board.java
@@ -1,10 +1,14 @@
 public class Board{
     private Square[][] board;
 
+    //methods for accessing and mutating board/Squares
+
     public Board(){
 	board = new Square[15][15];
 	int max = board.length - 1;
 	int midpt = max / 2;
+	int q1 = Math.floorDiv(midpt, 2);
+	int q3 = max - q1;
 	for(int row = 0; row < board.length; row++){
 	    for(int col = 0; col < board[0].length; col++){
 		if(row == 0 && col == 0 ||
@@ -29,7 +33,18 @@ public class Board{
 			 row == midpt - 2 && col == midpt + 2 ||
 			 row == midpt - 2 && col == midpt + 6){
 		    board[row][col] = new Square("triple letter");
-		}else if(false){
+		}else if(row == 0 && (col == q1 || col == q3) ||
+			 row == q1 - 1 && (col == midpt - 1 || col == midpt + 1) ||
+			 row == q1 && col == midpt ||
+			 row == max && (col == q1 || col == q3) ||
+			 row == q3 + 1 && (col == midpt - 1 || col == midpt + 1) ||
+			 row == q3 && col == midpt ||
+			 col == 0 && (row == q1 || row == q3) ||
+			 col == q1 - 1 && (row == midpt - 1 || row == midpt + 1) ||
+			 col == q1 && row == midpt ||
+			 col == max && (row == q1 || row == q3) ||
+			 col == q3 + 1 && (row == midpt - 1 || row == midpt + 1) ||
+			 col == q3 && row == midpt){
 		    board[row][col] = new Square("double letter");
 		}else if(row == col ||
 			 row == max - col){
@@ -37,7 +52,6 @@ public class Board{
 		}else{
 		    board[row][col] = new Square("regular");
 		}
-		//must: place triple letter squares
 		//opt: find better more condensed placement function/formula
 	    }
 	}
