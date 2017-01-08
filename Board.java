35b01260f3d413fbd8cbbc103819236485a56063
Winnie Chen <wchen10@stuy.edu>
2017-01-08 08:18:08 -0500

    premium squares (except for triple letters) now added to the Board constructor. working on triple letters. In extra week, find more condensed premium square placement method

--- Board.java
+++ Board.java
@@ -3,10 +3,42 @@ public class Board{
 
     public Board(){
 	board = new Square[15][15];
+	int max = board.length - 1;
+	int midpt = max / 2;
 	for(int row = 0; row < board.length; row++){
 	    for(int col = 0; col < board[0].length; col++){
-		board[row][col] = new Square("regular");
-		//must place premium squares
+		if(row == 0 && col == 0 ||
+		   row == 0 && col == midpt ||
+		   row == 0 && col == max ||
+		   row == midpt && col == 0 ||
+		   row == midpt && col == max ||
+		   row == max && col == 0 ||
+		   row == max && col == midpt ||
+		   row == max && col == max){
+		    board[row][col] = new Square("triple word");
+		}else if(row == midpt + 6 && col == midpt - 2 ||
+			 row == midpt + 6 && col == midpt + 2 ||
+			 row == midpt + 2 && col == midpt - 6 ||
+			 row == midpt + 2 && col == midpt - 2 ||
+			 row == midpt + 2 && col == midpt + 2 ||
+			 row == midpt + 2 && col == midpt + 6 ||
+			 row == midpt - 6 && col == midpt - 2 ||
+			 row == midpt - 6 && col == midpt + 2 ||
+			 row == midpt - 2 && col == midpt - 6 ||
+			 row == midpt - 2 && col == midpt - 2 ||
+			 row == midpt - 2 && col == midpt + 2 ||
+			 row == midpt - 2 && col == midpt + 6){
+		    board[row][col] = new Square("triple letter");
+		}else if(false){
+		    board[row][col] = new Square("double letter");
+		}else if(row == col ||
+			 row == max - col){
+		    board[row][col] = new Square("double word");
+		}else{
+		    board[row][col] = new Square("regular");
+		}
+		//must: place triple letter squares
+		//opt: find better more condensed placement function/formula
 	    }
 	}
     }
