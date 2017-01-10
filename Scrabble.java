fbad4fbf829a81ae4048540f6a0614a82799d0f7
Winnie Chen <wchen10@stuy.edu>
2017-01-10 06:21:42 -0500

    added if statement for interpreting player commands, but the addKeyListener(_) is not added to the correct object.

--- Scrabble.java
+++ Scrabble.java
@@ -1,14 +1,35 @@
 import java.util.*; //random, scanner, arraylist
 import java.io.*; //file, filenotfoundexception
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+//import java.awt.event.KeyEvent;
+//import java.awt.event.KeyListener;
 
-public class Scrabble{
+
+public class Scrabble implements KeyListener{
     public static ArrayList<String> dictWordList=new ArrayList<String>();
     private ArrayList<Player> players;
     private Board gameBoard;
     private TileBag tileBag;
     private String scorekeeper;
     private ArrayList<Integer> totalRoundScores;
+    private String currentInput;
     
+    public void keyPressed(KeyEvent e){
+	return;
+    }
+
+    public void keyReleased(KeyEvent e){
+	return;
+    }
+
+    public void keyTyped(KeyEvent e){
+	char key = e.getKeyChar();
+	currentInput += key;
+	System.out.println(currentInput);
+    }
+
     public void initializeArrayList(){
 	try{
 	    Scanner in = new Scanner(new File("WordList.txt"));
@@ -110,6 +131,10 @@ public class Scrabble{
 	tileBag.refillRack(players.get(1));
 	overwriteScorekeeper(false);
 	totalRoundScores = new ArrayList<Integer>(0);
+	currentInput = "";
+	//System.in.addKeyListener(this);
+	//Console b = new System.console();
+	//b.addKeyListener(b);
     }
 
     private static String welcomeInstructions(){
@@ -121,7 +146,7 @@ public class Scrabble{
     }
 
     private String rewriteGame(Player currentPlayer){
-        return gameBoard + "\n" + scorekeeper + "\nCurrent Player: " + currentPlayer.getName() + "\n" + currentPlayer + instructions();
+        return gameBoard + "\n" + scorekeeper + "\nCurrent Player: " + currentPlayer.getName() + "\n" + currentPlayer + "\n" + instructions();
     }
 
     private void overwriteGame(Player currentPlayer){
@@ -134,11 +159,44 @@ public class Scrabble{
     public void runGame(){
 	int round = 0;
 	int turn = 0;
+        //addKeyListener(this);
         while(true){ //looping through/counting rounds
 	    for(turn = 0; turn < players.size(); turn++){ //looping through players
 		overwriteGame(players.get(turn)); //print the display for currentPlayer
+		currentInput = "";
+		while(currentInput.length() == 0 ||
+		      (int) currentInput.charAt(currentInput.length() - 1) != KeyEvent.VK_ENTER){
+		    Cmd.wait(500);
+		}
+		String tempInput = currentInput;
+		System.out.println(tempInput);
 
+		if(tempInput.charAt(0) == '0'){
+		    players.get(turn).pass();
+		}else if((int) '1' <= (int)tempInput.charAt(0) &&
+			 (int) '7' >= (int)tempInput.charAt(0)){ //limit it to indexes of rack (depending on rack size)
+		    for(int i = 0; i < tempInput.length() - 1; i++){
+			if((int) '1' <= (int)tempInput.charAt(i) &&
+			   (int) '7' >= (int)tempInput.charAt(i)){
+			    players.get(turn).requestExchange(tileBag, Integer.parseInt(tempInput.substring(i, i + 1)));
+			}
+		    }
+		}else{
+		    int nextSpace = tempInput.indexOf(" ");
+		    String word = tempInput.substring(0, nextSpace);
+		    tempInput = tempInput.substring(nextSpace + 1);
+		    nextSpace = tempInput.indexOf(" ");
+		    int xCor = Integer.parseInt(tempInput.substring(0, nextSpace));
+		    tempInput = tempInput.substring(nextSpace + 1);
+		    nextSpace = tempInput.indexOf(" ");
+		    int yCor = Integer.parseInt(tempInput.substring(0, nextSpace));
+		    String dir = tempInput.substring(nextSpace + 1, nextSpace + 2);
+		    
+		    players.get(turn).placeWord(gameBoard, this, word, xCor, yCor, dir);
+		}
+		
 		//key(board)listener getting and storing the input
+		    //responses for incorrect input formatting
 		//calling the appropriate player method depending on input (placing the tile and dealing with premium effects will go here...or we can check at the end of a turn if the player placed tiles. it may also be better to do the scoring within the placing the tiles b/c those values are kept in player anyway.)
 		
 		
@@ -165,45 +223,59 @@ public class Scrabble{
     }
     
     //should we include a chart of letter values on the bottom or top of the screen?
-
-    //need the calculateScore method, but that depends on the words placed and intersections
-
-    //update wordValidityCheck() or do so in Player when placing word
+    //add to rules not to type anything unless your turn?
+    
+    //forgot to let differentiation occur in word placement if necessary
 
     
     
     public static void main(String[] args){
-	/*Scrabble a=new Scrabble();
+	/*Board testboard=new Board();
+	Scrabble a=new Scrabble();
 	a.initializeArrayList();
 	//System.out.println(a.dictWordList);
 	Tile b=new Tile("b");
 	Tile e=new Tile("e");
 	Tile d=new Tile("d");
-	Tile x=new Tile("x");
-	Tile y=new Tile("y");
-	Tile z=new Tile("z");
+	Tile t=new Tile("t");
+	Tile r=new Tile("r");
+	//Tile e2=new Tile("e");
+	Tile o=new Tile("o");
 	Tile blank=new Tile();
 	blank.differentiate("a");
+	Tile a2=new Tile("a");
 	TileBag tileBag=new TileBag();
 	Player jen=new Player("Jenny");
 	jen.addToRack(b);
 	jen.addToRack(e);
 	jen.addToRack(d);
-	jen.addToRack(x);
-	jen.addToRack(y);
-	jen.addToRack(z);
-	jen.addToRack(blank);
+	jen.addToRack(t);
+	jen.addToRack(r);
+	jen.addToRack(o);
+	//jen.addToRack(blank);
+	jen.addToRack(a2);
         System.out.println(jen);
 	tileBag.refillRack(jen);
 	System.out.println(jen);
 	//System.out.println(wordValidityCheck("bed"));
-        jen.placeWord(a,"bead",0,13,"v");
+        jen.placeWord(testboard,a,"bet",6,8,"v");
+	System.out.println("Round Score: "+jen.getRoundScore());
+	System.out.println("Total Score: "+jen.getTotalScore());
 	System.out.println(jen);
 	tileBag.refillRack(jen);
 	System.out.println(jen);
-	System.out.println(jen.getEndTurn());*/
+	System.out.println(jen.getEndTurn());
+	System.out.println(testboard);
+	jen.placeWord(testboard,a,"to",6,6,"h");
+	System.out.println("Round Score: "+jen.getRoundScore());
+	System.out.println("Total Score: "+jen.getTotalScore());
+	System.out.println(testboard);
+	System.out.println(jen);
+	jen.requestExchange(tileBag,3);
+	System.out.println(jen);*/
 	if(args.length == 3 && args[0].equals("default")){
 	    Scrabble a = new Scrabble(args[1], args[2]);
+	    //a.addKeyListener(this);
 	    a.runGame();
 	}else if(args.length < 3){
 	    System.out.println(welcomeInstructions());
